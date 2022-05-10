c2683c2eef3a5c02cecdebd0a9bf02adc3e4f079
moth1995 <marcos-flores@live.com.ar>
2022-05-10 04:06:03 -0300

    changes to make it easier to compile

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
 @echo on
-pyinstaller --onefile "fsrv_assigner.py" --name "fsrv_assigner" --noconsole  --version-file file_version_info.txt
+pyinstaller --onefile "fsrv_assigner.py" --name "fsrv_assigner" --noconsole --version-file file_version_info.txt --distpath="bin"
+Xcopy /E .\config\ .\bin\config\
 pause
