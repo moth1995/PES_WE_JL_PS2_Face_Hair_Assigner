69d603adcbc3b6909dc33ed14ca099d87419d259
moth1995 <marcos-flores@live.com.ar>
2022-05-10 04:47:30 -0300

    version bump and improving the make file

--- make.bat
+++ make.bat
@@ -1,4 +1,16 @@
 @echo on
-pyinstaller --onefile "fsrv_assigner.py" --name "fsrv_assigner" --noconsole --version-file file_version_info.txt --distpath="bin"
-Xcopy /E .\config\ .\bin\config\
+set PY_FILE=fsrv_assigner.py
+set PROJECT_NAME=fsrv_assigner
+set VERSION=1.3
+
+pyinstaller --onefile %PY_FILE% --name %PROJECT_NAME%_%VERSION% --noconsole --version-file file_version_info.txt
+
+Rem This command below is just specific for this script
+
+Xcopy /E ".\config\" ".\dist\config\"
+
+Rem end of extra command
+
+cd dist
+tar -acf %PROJECT_NAME%_%VERSION%_release.zip *
 pause
